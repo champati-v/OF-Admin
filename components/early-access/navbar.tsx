a17db91821040a61e48fdad69b4cf76954b2c5fa
vibekananda Champati <vibekanandac15@gmail.com>
2025-04-04 17:48:17 +0530

    added marketplace

--- components/early-access/navbar.tsx
+++ components/early-access/navbar.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { LogOut } from "lucide-react"
 import Image from "next/image"
 
 export function Navbar() {
